8aa31e86544738a50f1d94f418a93ebdaa6aef33
Diego Mateos <dimateos@ucm.es>
2020-02-07 11:42:14 +0100

    Structured project with dependencies

---
+++ VisualStudio/README.md
@@ -0,0 +1,23 @@
+# FOLDER STRUCTURE
+The visual studio solution is structured as follows:
+
+```
+/Dependencies 	-> dependecies lib and includes
+/Exes 		-> structured output with common assets
+/Inter 		-> intermediate compilation files
+/Projects 	-> visual studio projects
+/Src 		-> source code
+```
+
+* All visual studio temporal files are ignored
+* `/Dependencies` and `/Inter` are ignored
+* All `.dll` and `.exe` inside `/Exes` are ignored
+	* They share `/Assets`, thats why the whole folder cannot be ignored
+
+## DEPENDENCIES
+
+I provided an small `bin.zip` you can just unzip and merge all the folders to have the project compiling and running.
+
+Anyway you can add the dependencies yourself:
+* Add `glad` and `SDL2-2.0.10` inside `/Dependencies`
+* Add the corresponding SDL `.dll` to `/Exes/x64` and `/Exes/Win32`
---
+++ VisualStudio/Src/main.cpp
@@ -0,0 +1,87 @@
+﻿/// TFG_dimateos
+// Diego Mateos Arlanzon
+
+// WINDOWS
+#include <SDL.h>
+#include <glad\glad.h>
+#include <stdio.h>
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+
+int main(int argc, char* argv[]) {
+	printf("TFG - start app\n");
+	bool initFailure = false;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = SDL_CreateWindow(
+		"SDL + OpenGL",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		1280,
+		720,
+		SDL_WINDOW_OPENGL);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+		SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GLContext mainContext = SDL_GL_CreateContext(window);
+
+	// Only for Windows.
+	gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+	//if (initFailure) { printf("Init input failed... exit\n"); return 1; }
+	printf("GL VERSION %s\n", glGetString(GL_VERSION));
+
+	// Main loop
+	printf("TFG - start loop\n");
+	bool exit = false;
+	SDL_Event event;
+
+	//_deltaTimer.start();
+	while (!exit) {
+		// get elapsed time + wait for fps (if configured)
+		//delta = _deltaTimer.getDelta();
+		//#if DEBUG_FPS
+		//	printf("%f fps (%f delta)\n", 1.0 / delta, delta);
+		//#endif
+
+		//update game input logic state
+		//westBank.Input(input.getState());
+
+		//update and render game
+		//westBank.Update(delta);
+		//westBank.Render(&renderThread);
+
+		// Present + block if already max pending frames
+		//renderThread.enqueueCommand(renderCommand);
+		//while (renderThread.maxPendingFrames()) {
+		//	//PlatSleeping::platSleep(Config::sleepMaxPendingFrames_seconds);
+		//};
+
+		// handle exit + poll events (ps4) or emit input events (pc)
+		//exit = plat.Tick();
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				exit = true;
+			}
+		}
+
+		glClearColor(1.0f, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+	}
+	printf("TFG - end loop\n");
+
+	// Release
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	printf("TFG - end app\n");
+	return 0;
+}
